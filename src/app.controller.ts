45943f5df4b55b3938794754dc226149b800feb7
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-08-08 14:59:07 +0530

    all data

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Post()
+  add(@Req() req:Request){
+    return 'hi you are login success full'+" "+req.body.uname;
+  }
+
+}
